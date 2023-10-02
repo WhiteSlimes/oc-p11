f994044507c8e67f1617b5a39c3c08d53cacb972
WhiteSlimes <43409869+WhiteSlimes@users.noreply.github.com>
2023-10-02 14:36:51 +0200

    ~ Versioning 1.1 ~
    - Header
    - Banner
    - Features

--- projet/src/Pages/Home/Home.js
+++ projet/src/Pages/Home/Home.js
@@ -1,5 +1,25 @@
-function Home(){
+import React from "react"
+import Banner from "../../Components/Banner/Banner";
+import Feature from "../../Components/Feature/Feature";
+import "./HomeStyle.css"
 
+import iconChat from "../../Assets/icon-chat.png";
+import iconMoney from "../../Assets/icon-money.png";
+import iconSecurity from "../../Assets/icon-security.png";
+
+function Home(){
+    return(
+
+        <div>
+            <Banner />
+            <div className="Features">
+                <Feature iconSrc={iconChat} iconAlt="Chat Icon" title="You are our #1 prority" description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />
+                <Feature iconSrc={iconMoney} iconAlt="Chat Icon" title="You are our #1 prority" description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />
+                <Feature iconSrc={iconSecurity} iconAlt="Chat Icon" title="You are our #1 prority" description="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes." />
+            </div>
+        </div>
+
+    );
 }
 
 export default Home
